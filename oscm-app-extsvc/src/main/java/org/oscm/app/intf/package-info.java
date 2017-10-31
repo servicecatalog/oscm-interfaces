f97ae26638ecdb556e2bc1ff5455093c278bcad3
Dawid <dawid.chojnacki@ts.fujitsu.com>
2017-10-31 10:23:04 +0100

    mavenize external interfaces projects (#2)
    * Extracted extsvc projects.
    
    * Removing unnecessery projects
    
    * removal project folder
    
    * Modified artifactId.
    
    * Removing project files
    
    * oscm-app-extsvc v2 modified to versionless. Version is specified in POM now. Removed oscm-app-extsvc v1 project.
    
    * Removed unused plugin.
    
    * Removed unsed project.
    
    * Removed unsed project.
    
    * Removal implementations and added wsdl generations.

---
+++ oscm-app-extsvc/src/main/java/org/oscm/app/intf/package-info.java
@@ -0,0 +1,8 @@
+/* 
+ *  Copyright FUJITSU LIMITED 2017
+ */
+/**
+ * Provides the public utilities of APP and the 
+ * interface for implementing a service controller.
+ */
+package org.oscm.app.intf;
